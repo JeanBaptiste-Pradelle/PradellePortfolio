bf51235bb3692706fc9958327af11b280e0e2e56
Jean-Baptiste <jbpradelle@gmail.com>
2024-06-11 15:36:03 +0200

    chrome 2.1

--- src/composant/Home.js
+++ src/composant/Home.js
@@ -1,31 +1,32 @@
 import React, { useEffect } from "react";
 import "../css/home.css";
 import gsap from "gsap";
+import { FaLongArrowAltRight } from "react-icons/fa";
 
 function Home() {
   useEffect(() => {
     gsap.to(".presentation", {
-		startAt: {opacity:0},
+      startAt: { opacity: 0 },
       opacity: 1,
       x: 0,
-	duration: 5,
+      duration: 5,
     });
-  });
+  }, []);
 
   return (
-    <section id="Acceuil " className="h-100 w-100">
-     
-      <div className=" homeDiv  h-100 d-flex flex-column justify-content-center align-content-center   ">
-        <span className="presentation col-10 d-flex justify-content-center mx-auto fs-4 ">
-          Enchanté!
-          Mon nom est Jean-Baptiste, je suis un développeur full-stack basé à Clermont-Ferrand.
-          Passionné par le développement web depuis toujours je suis à votre disposition pour réaliser tout vos projets.
+    <section id="Acceuil" className="h-100 w-100">
+      <div className="homeDiv h-100 d-flex flex-column justify-content-center align-content-center">
+        <span className="presentation col-10 d-flex justify-content-center mx-auto fs-4">
+          Enchanté! Mon nom est Jean-Baptiste, je suis un développeur full-stack
+          basé à Clermont-Ferrand. Passionné par le développement web depuis
+          toujours je suis à votre disposition pour réaliser tout vos projets.{" "}
         </span>
+      </div>
+      <div className="arrow-container fw-bold ">
+        <FaLongArrowAltRight style={{height:"6em", width:"6em"}} />
       </div>
     </section>
   );
 }
-
-
 
 export default Home;
